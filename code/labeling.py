5f484017dea853d2530abf4fcc10c91a4fa01a6b
abigailhaddad <abigail.haddad@gmail.com>
2023-10-25 15:25:02 -0400

    -fix reading-in labeled csv to warn but not break if labels are missing

--- code/labeling.py
+++ code/labeling.py
@@ -1,3 +1,5 @@
+import csv
+
 class Labeler:
     def __init__(self, text_objs, base_task):
         self.text_objs = text_objs
@@ -10,25 +12,31 @@ class Labeler:
 
     def auto_label(self):
         for text_obj in self.text_objs:
-            if text_obj.category == "different_tasks" or text_obj.category == "others":
+            if text_obj.category in ["different_tasks", "others"]:
                 text_obj.label = 0
 
-    def manual_label(self):
-        for obj in self.text_objs:
-            # If it's already labeled or it's from "different_tasks" or "others", skip it
-            if obj.label is not None or obj.category in ["different_tasks", "others"]:
-                continue
-            
-            # Display the text
-            print(obj.text)
-            
-            # Ask for a label
-            label = input("Is this explaining the base task? (1 for Yes, 0 for No, s to skip): ")
-            if label == 's':
-                continue
-            
-            obj.label = int(label)
+    def export_to_csv(self, filename):
+        with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Text", "Category", "Label"])  # Header
+            for obj in self.text_objs:
+                writer.writerow([obj.text, obj.category, obj.label])
+
+    def import_from_csv(self, filename):
+        with open(filename, 'r', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)  # skip header row
+            for row, obj in zip(reader, self.text_objs):
+                _, _, label = row
+                if label:  # Check if the label is not empty
+                    obj.label = int(label)
+                else:
+                    print(f"Warning: Skipped an empty label for text: {obj.text}")
+
 
     def run(self):
         self.auto_label()
-        self.manual_label()
+        self.export_to_csv('../results/texts_for_labeling.csv')
+        # You will manually label using the exported CSV here.
+        input("Please manually label the data in 'texts_for_labeling.csv'. Press Enter when done.")
+        self.import_from_csv('../results/texts_for_labeling.csv')
--- code/main.py
+++ code/main.py
@@ -12,4 +12,3 @@ if __name__ == "__main__":
     for text in generated_texts_result:
         print(text.category)
         print(text.label)
-    
