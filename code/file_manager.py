f8da4770d78a8440fa00e25859a8128058957031
abigailhaddad <abigail.haddad@gmail.com>
2023-11-06 19:40:09 -0500

    make load_json specify utf-8

--- code/file_manager.py
+++ code/file_manager.py
@@ -8,5 +8,6 @@ class FileManager:
     
     @staticmethod
     def load_from_json(file_path: str):
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             return json.load(file)
+
